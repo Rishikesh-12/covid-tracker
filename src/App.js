2778a7a218b0a3633c3794fb6ad7da5a134962c2
Rishikesh <contactrishikeshhere@gmail.com>
2020-07-21 23:16:02 +0530

    checking error with deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Cards, CountryPicker, Chart, Developer } from './components';
+import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
 
@@ -33,7 +33,7 @@ class App extends React.Component {
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
-        <Developer className="git"/> 
+        {/* <Developer className="git"/>  */}
       </div>
     );
   }
